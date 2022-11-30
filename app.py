7d1c36497e1443db6c02e706bd7c60aafda3ae2d
gustavolopesmartins <gustavolopes.br@abrasel.com.br>
2022-11-30 11:44:32 -0300

    .
    Signed-off-by: gustavolopesmartins <gustavolopes.br@abrasel.com.br>

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ all_files = list(filter(lambda x: '.csv' in x, os.listdir(diretorio)))
 
 os.system('cls')
 
-for file in all_files:
-    try:
-        Extracao_CNAE(diretorio=diretorio,file=file)
-    except:
-        time.sleep(10)
-        Extracao_CNAE(diretorio=diretorio,file=file)
-#Extracao_CNAE(diretorio=diretorio, file= file)
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y0.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y2.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y3.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y4.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y6.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y7.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y8.D21119.csv')
+Extracao_CNAE(diretorio=diretorio,file="K3241.K03200Y9.D21119.csv")
