898ce151509eca3bdcca88de81c8a52f3c6753d6
gustavolopesmartins <gustavolopes.br@abrasel.com.br>
2022-11-30 12:56:37 -0300

    .
    Signed-off-by: gustavolopesmartins <gustavolopes.br@abrasel.com.br>

---
+++ contagem_teste.py
@@ -0,0 +1,33 @@
+# ler todos os arquivos csv do diretório e guardar em um objeto
+# import required modules
+import numpy as np
+import time
+import os
+import re
+import pandas as pd
+from dask import dataframe as dd
+
+diretorio = r'Bases/'
+all_files = list(filter(lambda x: '.csv' in x, os.listdir(diretorio)))
+
+#Warnings: Possui uma série de funções e comandos para tratamento de mensagens de avisos e alertas do Python
+import warnings
+warnings.filterwarnings("ignore")
+
+#Comando para exibir todas colunas do arquivo
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+for file in all_files:
+    print(f'Operando arquivo {file}')
+    
+    with open(f"{diretorio}/{file}", mode='r', encoding='utf-8', errors='ignore') as arq:
+        linhas = len(arq.readlines())
+        arq.close()
+    
+    if (linhas%2) == 0:
+        print(linhas)
+        print(f"Número impar de linhas no arquivo {file}")
+    else:
+        print(f"Número par de linhas no arquivo {file}")
+        print(linhas)
