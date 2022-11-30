d365fd76525938aebe082a8636f4d5ae3f614241
gustavolopesmartins <gustavolopes.br@abrasel.com.br>
2022-11-30 09:18:22 -0300

    Inclusão dos arquivos de manipulação de df
    Signed-off-by: gustavolopesmartins <gustavolopes.br@abrasel.com.br>

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import csv
+import time
+from src import Extracao_CNAE
+diretorio = r'C:\Users\ABRASEL NACIONAL\Documents\CNPJ_PROGRAMATICA\ESTABELECIMENTOSCSV/'
+all_files = list(filter(lambda x: '.csv' in x, os.listdir(diretorio)))
+
+os.system('cls')
+
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y0.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y2.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y3.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y4.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y6.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y7.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y8.D21119.csv')
+Extracao_CNAE(diretorio=diretorio, file='K3241.K03200Y9.D21119.csv')
+#Extracao_CNAE(diretorio=diretorio, file= file)
