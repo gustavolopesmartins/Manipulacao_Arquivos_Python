920c7336d4c24ac57ccec92fd0c164bfabc5ea95
gustavolopesmartins <gustavolopes.br@abrasel.com.br>
2022-12-06 13:59:01 -0300

    .
    Signed-off-by: gustavolopesmartins <gustavolopes.br@abrasel.com.br>

--- utilitarios/app.py
+++ utilitarios/app.py
@@ -1,17 +1,21 @@
 import os
 import csv
 import time
-from utilitarios.src import Extracao_CNAE
+from src import Extracao_CNAE
 diretorio = r'C:\Users\ABRASEL NACIONAL\Documents\CNPJ_PROGRAMATICA\ESTABELECIMENTOSCSV/'
 all_files = list(filter(lambda x: '.csv' in x, os.listdir(diretorio)))
 
 os.system('cls')
+#from convertearquivo import convertearquivo
+#convertearquivo(diretorio=diretorio, tipo_atual='ESTABELE',tipo_destino='csv')
 
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y0.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y2.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y3.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y4.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y6.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y7.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file='K3241.K03200Y8.D21119.csv')
-#Extracao_CNAE(diretorio=diretorio,file="K3241.K03200Y9.D21119.csv")
+#Extracao_CNAE(diretorio=diretorio,file=all_files[0])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[1])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[2])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[3])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[4])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[5])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[6])
+#Extracao_CNAE(diretorio=diretorio,file=all_files[7])
+Extracao_CNAE(diretorio=diretorio,file=all_files[8])
+Extracao_CNAE(diretorio=diretorio,file=all_files[9])
